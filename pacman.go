fe90d04e47806247c218a084c3a343979171f45f
Camille Jackson <cj@mail.co.de>
2020-07-15 18:13:35 -0400

    satisfy linter

--- pacman.go
+++ pacman.go
@@ -10,10 +10,10 @@ import (
 	"sync"
 )
 
-//	Empty struct takes no space in memory.
-//	Use this as the value in the dependencies map since there's no information
-//	to associate with each dependency, other than its presence in the map.
-//	This is a fake Set in Go.
+// Empty struct takes no space in memory.
+// Use this as the value in the dependencies map since there's no information
+// to associate with each dependency, other than its presence in the map.
+// This is a fake Set in Go.
 var null struct{}
 
 type command string
@@ -23,8 +23,8 @@ type pkg string
 
 type dependencies map[pkg]struct{}
 
-//  index of packages
-//  Each key is a registered package, and each value is that package's list of dependencies.
+// index of packages
+// Each key is a registered package, and each value is that package's list of dependencies.
 type database map[pkg]dependencies
 
 type syncDatabase struct {
@@ -45,7 +45,7 @@ const (
 	errorRes result = "ERROR\n"
 )
 
-//  Stand up TCP server and start handling connections.
+// Stand up TCP server and start handling connections.
 func main() {
 	listener, err := net.Listen(protocol, address)
 	if err != nil {
@@ -63,14 +63,14 @@ func main() {
 			log.Fatalln("Error while accepting connection: ", err.Error())
 		}
 
-		//	a goroutine is a function that is capable of running concurrently with other functions.
-		//	Normally when we invoke a function our program will execute all the statements in a function and then return to the next line following the invocation.
-		//	With a goroutine we return immediately to the next line and don't wait for the function to complete.
+		// a goroutine is a function that is capable of running concurrently with other functions.
+		// Normally when we invoke a function our program will execute all the statements in a function and then return to the next line following the invocation.
+		// With a goroutine we return immediately to the next line and don't wait for the function to complete.
 		go handleConnection(conn, &db)
 	}
 }
 
-//  Handle any number of requests on a connection and clean up after it.
+// Handle any number of requests on a connection and clean up after it.
 func handleConnection(conn net.Conn, db *syncDatabase) {
 	defer conn.Close()
 
@@ -84,7 +84,7 @@ func handleConnection(conn net.Conn, db *syncDatabase) {
 	}
 }
 
-//  Read a message off the connection, parse it, perform core operation.
+// Read a message off the connection, parse it, perform core operation.
 func handleRequest(conn net.Conn, db *syncDatabase, reader *bufio.Reader) error {
 	message, err := reader.ReadString('\n')
 	if err != nil {
@@ -137,11 +137,11 @@ func parseCommand(str string) (command, error) {
 	}
 }
 
-//	Take in a string and return a slice of pkgs.
+// Take in a string and return a slice of pkgs.
 func parseDeps(str string) []pkg {
 	deps := []pkg{}
 
-	//  Split 'str' on comma characters and filter empty strings out of resulting array.
+	// Split 'str' on comma characters and filter empty strings out of resulting array.
 	parsed := strings.Split(str, ",")
 	var compacted []string
 	for _, element := range parsed {
@@ -150,14 +150,14 @@ func parseDeps(str string) []pkg {
 		}
 	}
 
-	//  Construct a package for each dependency, appending the new package to the list of dependencies.
+	// Construct a package for each dependency, appending the new package to the list of dependencies.
 	for _, dep := range compacted {
 		deps = append(deps, pkg(dep))
 	}
 	return deps
 }
 
-//	Divide message into its constituent data types.
+// Divide message into its constituent data types.
 func parse(message string) (command, pkg, []pkg, error) {
 	var cmd command
 	var candidate pkg
@@ -185,37 +185,37 @@ func parse(message string) (command, pkg, []pkg, error) {
 	return cmd, candidate, deps, nil
 }
 
-//	Attempt to index a pkg and its deps to the db, return a response code.
+// Attempt to index a pkg and its deps to the db, return a response code.
 func index(db database, candidate pkg, deps []pkg) result {
 	newDeps := make(dependencies)
 
 	for _, dep := range deps {
 		_, exists := db[dep]
-		//	Return `FAIL\n` if the candidate cannot be indexed because some of its dependencies aren't indexed yet.
+		// Return `FAIL\n` if the candidate cannot be indexed because some of its dependencies aren't indexed yet.
 		if !exists {
 			return failRes
 		}
-		//	Move deps that do already exist in db from slice of pkgs into newDeps map.
+		// Move deps that do already exist in db from slice of pkgs into newDeps map.
 		newDeps[dep] = null
 	}
 
-	//	Index candidate and its newDeps into db.
-	//	If a candidate already existed in db, update its list of dependencies to the one provided with the latest command.
+	// Index candidate and its newDeps into db.
+	// If a candidate already existed in db, update its list of dependencies to the one provided with the latest command.
 	db[candidate] = newDeps
-	//	Return `OK\n` if the candidate could be indexed.
+	// Return `OK\n` if the candidate could be indexed.
 	return okRes
 }
 
-//	Attempt to remove a pkg from the db, return a response code.
+// Attempt to remove a pkg from the db, return a response code.
 func remove(db database, candidate pkg) result {
 
-	//	Return `OK\n` if the candidate wasn't indexed.
+	// Return `OK\n` if the candidate wasn't indexed.
 	_, exists := db[candidate]
 	if !exists {
 		return okRes
 	}
 
-	//	Return `FAIL\n` if the candidate could not be removed from the index because some other indexed package depends on it.
+	// Return `FAIL\n` if the candidate could not be removed from the index because some other indexed package depends on it.
 	for _, deps := range db {
 		_, exists := deps[candidate]
 		if exists {
@@ -223,21 +223,21 @@ func remove(db database, candidate pkg) result {
 		}
 	}
 
-	//	Remove candidate from db.
+	// Remove candidate from db.
 	delete(db, candidate)
 	//	Return `OK\n` if the candidate could be removed.
 	return okRes
 }
 
-//	Look up a pkg in the db, return a response code.
+// Look up a pkg in the db, return a response code.
 func query(db database, candidate pkg) result {
 	_, exists := db[candidate]
 
-	//	Return `FAIL\n` if the candidate isn't indexed.
+	// Return `FAIL\n` if the candidate isn't indexed.
 	if !exists {
 		return failRes
 	}
 
-	//	Return `OK\n` if the candidate exists in the db.
+	// Return `OK\n` if the candidate exists in the db.
 	return okRes
 }
